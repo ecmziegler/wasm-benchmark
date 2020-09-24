5d686596fe947d11254ae8e58c19c22d1d83b334
Emanuel Ziegler <ecmziegler@chromium.org>
2020-09-24 17:22:00 +0200

    Distinguish in between compression and decompression in zlib benchmark

--- benchmarks/zlib/zlib_bench.c
+++ benchmarks/zlib/zlib_bench.c
@@ -14,51 +14,39 @@
 
 
 // don't inline, to be friendly to js engine osr
-void __attribute__ ((noinline)) doit(unsigned char *buffer, int size, int i) {
-  static unsigned char *buffer2 = NULL;
-  static unsigned char *buffer3 = NULL;
+void __attribute__ ((noinline)) do_compress(unsigned char* uncompressed_buffer, unsigned long uncompressed_size, unsigned char* compressed_buffer, unsigned long* compressed_size) {
+  compress(compressed_buffer, compressed_size, uncompressed_buffer, uncompressed_size);
+}
 
-  unsigned long maxCompressedSize = compressBound(size);
-
-  if (!buffer2) buffer2 = (unsigned char*)malloc(maxCompressedSize);
-  if (!buffer3) buffer3 = (unsigned char*)malloc(size);
-
-  unsigned long compressedSize = maxCompressedSize;
-  compress(buffer2, &compressedSize, buffer, size);
-  if (i == 0) printf("sizes: %d,%d\n", size, (int)compressedSize);
-
-  unsigned long decompressedSize = size;
-  uncompress(buffer3, &decompressedSize, buffer2, (int)compressedSize);
-  assert(decompressedSize == size);
-  wasm_perf_record_relative_progress("compress-decompress", size);
-  if (i == 0) {
-    if (strcmp((char*)buffer, (char*)buffer3) != 0) {
-      puts("incorrect output!");
-      abort();
-    }
-    puts("output looks good");
-  }
+void __attribute__ ((noinline)) do_decompress(unsigned char* compressed_buffer, unsigned long compressed_size, unsigned char* uncompressed_buffer, unsigned long* uncompressed_size) {
+  uncompress(uncompressed_buffer, uncompressed_size, compressed_buffer, compressed_size);
 }
 
 int main(int argc, char **argv) {
-  int size, iters;
-  int arg = argc > 1 ? argv[1][0] - '0' : 3;
+  unsigned long uncompressed_size = 100000;
+  int iters;
+  int enable_compress = argc <= 1 || strncmp(argv[1], "compress", 9) == 0;
+  int enable_decompress = argc <= 1 || strncmp(argv[1], "decompress", 11) == 0;
+  int arg = argc > 2 ? argv[2][0] - '0' : 3;
   switch(arg) {
     case 0: return 0; break;
-    case 1: size = 100000; iters = 60; break;
-    case 2: size = 100000; iters = 250; break;
-    case 3: size = 100000; iters = 500; break;
-    case 4: size = 100000; iters = 5*500; break;
-    case 5: size = 100000; iters = 10*500; break;
+    case 1: iters = 60; break;
+    case 2: iters = 250; break;
+    case 3: iters = 500; break;
+    case 4: iters = 5*500; break;
+    case 5: iters = 10*500; break;
     default: printf("error: %d\\n", arg); return -1;
   }
 
-  unsigned char *buffer = (unsigned char*)malloc(size);
+  unsigned long maxCompressedSize = compressBound(uncompressed_size);
+  unsigned long compressed_size = maxCompressedSize;
+  unsigned char* uncompressed_buffer = (unsigned char*)malloc(uncompressed_size);
+  unsigned char* compressed_buffer = (unsigned char*)malloc(maxCompressedSize);
 
   int i = 0;
   int run = 0;
   char runChar = 17;
-  while (i < size) {
+  while (i < uncompressed_size) {
     if (run > 0) {
       run--;
     } else {
@@ -69,17 +57,30 @@ int main(int argc, char **argv) {
         runChar = (i*i) % 6714;
       }
     }
-    buffer[i] = runChar;
+    uncompressed_buffer[i] = runChar;
     i++;
   }
 
-  wasm_perf_record_relative_progress("compress-decompress", 0);
-  for (i = 0; i < iters; i++) {
-    doit(buffer, size, i);
+  if (enable_compress) {
+    for (i = 0; i < iters; i++) {
+      wasm_perf_record_progress("compress", i);
+      do_compress(uncompressed_buffer, uncompressed_size, compressed_buffer, &compressed_size);
+    }
+    wasm_perf_record_progress("compress", iters);
+  } else if (enable_decompress) {
+    do_compress(uncompressed_buffer, uncompressed_size, compressed_buffer, &compressed_size);
+  }
+  printf("sizes: %d,%d\n", (int)uncompressed_size, (int)compressed_size);
+
+  if (enable_decompress) {
+    for (i = 0; i < iters; i++) {
+      wasm_perf_record_progress("decompress", i);
+      do_decompress(compressed_buffer, compressed_size, uncompressed_buffer, &uncompressed_size);
+    }
+    wasm_perf_record_progress("decompress", iters);
   }
 
   printf("ok.\n");
 
   return 0;
 }
-
